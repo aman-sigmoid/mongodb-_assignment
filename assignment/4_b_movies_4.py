523a787810ac9fe90825510064d1b0339bed27bd
aman-sigmoid <98140653+aman-sigmoid@users.noreply.github.com>
2022-02-09 13:57:08 +0530

    Add files via upload

--- assignment/4_b_movies_4.py
+++ assignment/4_b_movies_4.py
@@ -13,12 +13,47 @@ n=10
 # Find top `N` movies for each genre with the highest IMDB rating
 
 result= db.movies.aggregate([
-    {"$unwind": "$genres"},
-    {"$project": {"rating": "$imdb.rating","genres": "$genres", "title": "$title"}},
-    {"$group": {"_id": {"genres": "$genres", "max_rating": {"$max": "$rating"}, "title": {"first": "$title"}}}},
-    {"$sort": {"_id.max_rating": -1}},
-    {"$limit": n}
-])
+        {
+            '$unwind': {
+                'path': '$genres',
+                'preserveNullAndEmptyArrays': False
+            }
+        }, {
+            '$project': {
+                'title': 1,
+                'rating': '$imdb.rating',
+                'genres': 1
+            }
+        }, {
+            '$sort': {
+                'rating': -1
+            }
+        }, {
+            '$match': {
+                'rating': {
+                    '$ne': ''
+                }
+            }
+        }, {
+            '$group': {
+                '_id': '$genres',
+                'movies': {
+                    '$push': {
+                        'movie': '$title',
+                        'rating': '$rating'
+                    }
+                }
+            }
+        }, {
+            '$project': {
+                'movies': {
+                    '$slice': [
+                        '$movies', 0, n
+                    ]
+                }
+            }
+        }
+    ])
 
 
 for res in result:
